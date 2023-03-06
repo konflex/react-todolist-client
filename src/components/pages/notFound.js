853cf6a9d466cb50b380d8b5a105e443e9b92001
FPC <francois-philippe@olenergies.com>
2023-03-06 16:52:43 +0100

    404 on mobile

--- src/components/pages/notFound.js
+++ src/components/pages/notFound.js
@@ -9,15 +9,13 @@ import Container
  from '../layout/containers'
 const NotFound = () => {
 	return(
-		<Container>
-			<div className="has-text-centered">
-				<h1 className="title is-3 has-text-weight-normal mb-0">Whoops</h1>
-				<h1 className="title is-1 mb-0" style={{ fontSize: "10rem"}}>404</h1>
-				<h1 className="title is-3 has-text-weight-normal">Page not found!</h1>
-
-				<p classNAme="is-1">We can't found the page you're looking for.</p>
-
-				<a className="button is-link  mt-4" href="/">
+		<Container className='not-found-container'>
+			<div className="">
+				<h1 className="">Whoops</h1>
+				<h1 className="not-found">404</h1>
+				<h1 className="">Page not found!</h1>
+				<p classNAme="">We can't found the page you're looking for.</p>
+				<a className="" style={{ color: 'white', }} href="/">
 					<strong>Go back</strong>
 				</a>
 			</div>
